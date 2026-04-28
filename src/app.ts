662d6721a2c8075f12d2f0f68f8415491c8a2c59
Eric Ibu <ericomartin.0785@gmail.com>
2026-04-28 12:02:22 +0100

    chore: unit & integration testing

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,12 @@ import cardRoutes from "./routes/card.routes";
 const app = express();
 
 app.use(express.json());
-app.use(errorHandler);
 app.use("/api/v1/cards", cardRoutes);
 
 app.get("/api/v1/health", (req, res) => {
   res.status(200).json({ status: "OK" });
 });
+
+app.use(errorHandler);
 
 export default app;
