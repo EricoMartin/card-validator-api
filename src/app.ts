8e202a8d04856dcf9674d0e03b7a8bdf20200a85
Eric Ibu <ericomartin.0785@gmail.com>
2026-04-28 09:20:19 +0100

    chore: edit app.ts to use routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import { errorHandler } from "./middlewares/error.middleware";
+import cardRoutes from "./routes/card.routes";
 
 const app = express();
 
 app.use(express.json());
+app.use(errorHandler);
+app.use("/api/v1/cards", cardRoutes);
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
